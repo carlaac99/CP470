d24484d78cfaa3862d01862484d6eb1c2a71a888
carlaac99 <cast473-@mylaurier.ca>
2020-10-11 12:43:26 -0400

    shared preferences added to login activity

--- app/src/main/java/com/example/androidassignments/LoginActivity.java
+++ app/src/main/java/com/example/androidassignments/LoginActivity.java
@@ -2,13 +2,61 @@ package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "LoginActivity";
+    private Object DefaultEmail;
+    //private Button button;
+    private EditText EditText;
+    SharedPreferences shared = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final String defaultEmail = shared.getString("DefaultEmail", "email@domain.com");
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+    public void saveEmail(View view){
+        final String userEmail = shared.getString("DefaultEmail",EditText.toString());
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
 }
